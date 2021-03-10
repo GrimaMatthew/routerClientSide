dffd47e0ea9321b56a9f26a72508be9df14f378b
GrimaMatthew <56166230+GrimaMatthew@users.noreply.github.com>
2021-03-10 13:11:14 +0100

    SRC_public_RouterVUE
    Creating Website

---
+++ src/components/TaskCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul class="list-group list-group-flush">
+    <router-link class="task-link" :to="{name: 'TaskDetails', params:{id: task.id}}">
+    <li class="list-group-item"><b>{{ task.title }}</b> on the {{task.date}} @ {{task.time}}</li>
+    </router-link>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'Task Card',
+
+  props: {
+    task: Object
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+a {
+  color: #42b983;
+}
+
+.card{
+    width: 95%;
+    margin: auto;
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <button type="button" class="btn btn-dark">Long</button>
+
+  </div>
+</template>
